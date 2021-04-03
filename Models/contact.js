61935dd5309c1bbb0f02acf5fa7a15f5db91dc6a
jaisydh07 <jasmailkaur.dhaliwal@dcmail.ca>
2021-04-02 22:19:54 -0400

    added Contact Model

---
+++ Models/contact.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ContactSchema = new Schema({
+    FullName: String,
+    EmailAddress: String,
+    ContactNumber: String
+}, {
+    collection: "contacts"
+});
+const Model = mongoose.model("Contact", ContactSchema);
+module.exports.Model = Model;
+//# sourceMappingURL=contact.js.map
