18f3a8c6aa145200d211a6fcfad4e00c46802b92
jaisydh07 <jasmailkaur.dhaliwal@dcmail.ca>
2021-04-02 21:58:33 -0400

    added mongoose

---
+++ Config/db.js
@@ -0,0 +1,8 @@
+"use strict";
+let mongoDBPath = "mongodb://localhost/webd6201";
+let sessionSecret = "someSecret";
+module.exports = {
+    Path: mongoDBPath,
+    Secret: sessionSecret
+};
+//# sourceMappingURL=db.js.map
